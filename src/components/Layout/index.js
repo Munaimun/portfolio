fa7bb3d8b29b8a539033cfb582a3721361f8e716
Munaimun <munaimunz@gmail.com>
2023-11-08 15:30:46 +0600

    logo changed

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,10 +2,17 @@ import { Outlet } from 'react-router-dom'
 import Projects from '../Projects'
 import SideBar from '../Sidebar'
 import './index.scss'
+import { Helmet } from 'react-helmet'
+import favicon from './favicon.png'
 
 const Layout = () => {
   return (
     <div className="App">
+      <Helmet>
+        <title>Munaimun Bari Fahad</title>
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+        {/* Additional meta tags or links if needed */}
+      </Helmet>
       <SideBar />
       <div className="page">
         <span className="tags top-tags">&lt;body&gt;</span>
